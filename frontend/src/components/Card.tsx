a9debf43f4b3c3568948149d704918cb28a39623
Alejandro Cano <alejandroyunes@outlook.com>
2024-07-22 22:14:11 -0500

    adding styles for card

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,14 +1,31 @@
 export default function Card() {
   // Please fetch the user data from the API
 
-  return <section className="w-full h-full p-5">
+  return <section className="w-full h-full p-4">
     <article className="max-w-screen-xl mx-auto bg-gray-200 rounded-lg">
 
       <div className="flex items-center justify-between md:flex-row flex-col">
 
-        <div className="space-y-2">
-          <h1 className="text-2xl">Alejandro</h1>
-          <p>medellin</p>
+        <div className="space-y-2 flex flex-row items-center">
+          <img src="https://picsum.photos/200/300" alt="avatar" className="object-cover w-12 h-12 rounded-full mr-4 ml-4" />
+          <div className="mr-4">
+            <h1 className="text-lg">Diego Segura</h1>
+            <p className="text-gray-500 text-xs">New York, NY</p>
+          </div>
+
+          <div className="flex items-center mr-4 border border-gray-300 rounded-lg p-2 bg-gray-300">
+            <img src="/icons/instagram.svg" alt="arrow-right" className="w-4 h-4 mr-2" />
+            <span className="text-gray-500 text-xs">
+              Ver perfil
+            </span>
+          </div>
+
+          <div className="flex items-center mr-4 border border-gray-300 rounded-lg p-2 bg-gray-300">
+            <img src="/icons/tiktok.svg" alt="arrow-right" className="w-4 h-4 mr-2" />
+            <span className="text-gray-500 text-xs">
+              Ver perfil
+            </span>
+          </div>
         </div>
 
         <div>
