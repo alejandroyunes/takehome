19a1e6c03c9c1f481f23e5269639af90596b1067
Alejandro Cano <alejandroyunes@outlook.com>
2024-07-23 18:52:53 -0500

    fixing styles

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,30 +2,47 @@ export default function Card() {
   // Please fetch the user data from the API
 
   return <section className="w-full h-full p-4">
-    <article className="max-w-screen-xl mx-auto bg-gray-200 rounded-lg">
+    <article className="max-w-screen-xl mx-auto bg-gray-100 rounded-lg">
 
       <div className="flex items-center justify-between md:flex-row flex-col">
 
-        <div className="space-y-2 flex flex-row items-center">
-          <img src="https://picsum.photos/200/300" alt="avatar" className="object-cover w-12 h-12 rounded-full mr-4 ml-4" />
-          <div className="mr-4">
-            <h1 className="text-lg">Diego Segura</h1>
-            <p className="text-gray-500 text-xs">New York, NY</p>
+        <div className="mt-4">
+
+          <div className="flex flex-row items-center">
+            <img src="https://picsum.photos/200/300" alt="avatar" className="object-cover w-8 h-8 rounded-full mr-2 ml-4" />
+
+            <div className="mr-12">
+              <h1 className="text-lg font-semibold">Diego Segura</h1>
+              <p className="text-gray-500 text-xs">New York, NY</p>
+            </div>
+
+            <div className="flex items-center mr-4 border border-gray-300 rounded-lg p-1 px-2 bg-gray-200">
+              <img src="/icons/instagram.svg" alt="arrow-right" className="w-4 h-4 mr-2" />
+              <span className="text-gray-700 text-xs font-semibold">
+                Ver perfil
+              </span>
+            </div>
+
+            <div className="flex items-center mr-4 border border-gray-300 rounded-lg p-1 px-2 bg-gray-200">
+              <img src="/icons/tiktok.svg" alt="arrow-right" className="w-4 h-4 mr-2" />
+              <span className="text-gray-700 text-xs font-semibold">
+                Ver perfil
+              </span>
+            </div>
           </div>
 
-          <div className="flex items-center mr-4 border border-gray-300 rounded-lg p-2 bg-gray-300">
-            <img src="/icons/instagram.svg" alt="arrow-right" className="w-4 h-4 mr-2" />
-            <span className="text-gray-500 text-xs">
-              Ver perfil
-            </span>
+          <div>
+            <p className="text-gray-700 text-xs ml-14 mt-2">
+              writer and designer
+            </p>
+            <p className="text-gray-700 text-xs ml-14">
+              rsvp below <span className="text-gray-500">@terms_eccles</span> on tax day
+            </p>
+            <p className="text-gray-700 text-xs ml-14">
+              follow for event updates
+            </p>
           </div>
 
-          <div className="flex items-center mr-4 border border-gray-300 rounded-lg p-2 bg-gray-300">
-            <img src="/icons/tiktok.svg" alt="arrow-right" className="w-4 h-4 mr-2" />
-            <span className="text-gray-500 text-xs">
-              Ver perfil
-            </span>
-          </div>
         </div>
 
         <div>
@@ -34,7 +51,7 @@ export default function Card() {
 
       </div>
 
-      <div className="flex space-x-2 mt-4">
+      <div className="flex space-x-2 mt-4 bg-gray-200 rounded-bl-lg rounded-br-lg">
         <button className="px-4 py-2 bg-blue-500 text-white rounded">Accept</button>
         <button className="px-4 py-2 bg-red-500 text-white rounded">Reject</button>
       </div>
