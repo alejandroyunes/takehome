416f03d7610da02d1444a589975ec7316471fb0e
Alejandro Cano <alejandroyunes@outlook.com>
2024-07-24 00:39:29 -0500

    fixing styles

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -8,7 +8,7 @@ export default function Card() {
 
         <div className="mt-4 w-full xs:w-1/2 md:w-full">
 
-          <div className="flex items-center justify-between border border-yellow-500">
+          <div className="flex items-center justify-between">
 
             <div className="flex flex-row items-center">
               <img src="https://picsum.photos/200/300" alt="avatar" className="object-cover w-8 h-8 rounded-full mr-2 ml-4" />
@@ -21,14 +21,14 @@ export default function Card() {
 
             <div className="flex">
 
-              <div className="flex mr-4 rounded-xl p-1 px-3 bg-gray-200">
+              <div className="flex mr-4 rounded-full p-2 px-4 bg-gray-200">
                 <img src="/icons/instagram.svg" alt="instagram icon" className="w-4 h-4 sm:mr-2" />
                 <span className="text-gray-700 text-xs font-semibold hidden sm:block">
                   Ver perfil
                 </span>
               </div>
 
-              <div className="flex mr-4 rounded-xl p-1 px-3 bg-gray-200">
+              <div className="flex mr-4 rounded-full p-2 px-4 bg-gray-200">
                 <img src="/icons/tiktok.svg" alt="tiktok icon" className="w-4 h-4 sm:mr-2" />
                 <span className="text-gray-700 text-xs font-semibold hidden sm:block">
                   Ver perfil
@@ -39,34 +39,35 @@ export default function Card() {
           </div>
 
           <div>
-            <div className="border border-blue-500">
+            <div>
               <p className="text-gray-700 text-xs ml-14 mt-2">
                 writer and designer
               </p>
               <p className="text-gray-700 text-xs ml-14">
-                rsvp below <span className="text-gray-500">@terms_eccles</span> on tax day
+                rsvp below <span className="text-gray-600 underline cursor-pointer">@terms_eccles</span> on tax day
               </p>
               <p className="text-gray-700 text-xs ml-14">
                 follow for event updates
               </p>
             </div>
 
-            <div className="border border-red-500">
+            <div className="flex flex-row justify-between">
               <div>
                 <p className="text-gray-800 font-semibold text-xs ml-14 mt-2">
                   High Potential | <span className="text-gray-500 hover:text-gray-700 underline cursor-pointer">View Application</span>
                 </p>
-              </div>
 
-              <div className="flex flex-row justify-between items-center mt-2">
                 <p className="text-gray-800 font-semibold text-xs ml-14">
                   ***********************
                 </p>
+              </div>
 
-                <button className="text-gray-800 font-semibold text-xs ml-14 mt-2 bg-gray-200 p-1 px-2 rounded-xl">
+              <div className="flex flex-row justify-between items-center mr-4">
+                <button className="text-gray-800 font-semibold text-xs bg-gray-200 px-2 py-1 rounded-full">
                   Customer
                 </button>
               </div>
+
             </div>
           </div>
 
@@ -78,9 +79,9 @@ export default function Card() {
 
       </div>
 
-      <div className="flex space-x-2 p-2 bg-gray-200 rounded-bl-lg rounded-br-lg border border-blue-400">
-        <button className="px-4 py-2 bg-blue-500 text-white rounded">Accept</button>
-        <button className="px-4 py-2 bg-red-500 text-white rounded">Reject</button>
+        <div className="flex space-x-2 p-2 bg-gray-200 rounded-bl-lg rounded-br-lg">
+        <button className="flex justify-center items-center px-3 py-2 bg-green-200 text-green-800 rounded-full">Accept <span className="ml-2"><img src="/icons/chevronDown.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
+        <button className="flex justify-center items-center px-3 py-2 bg-red-200 text-red-800 rounded-full">Reject <span className="ml-2"><img src="/icons/stopSvg.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
       </div>
 
     </article>
