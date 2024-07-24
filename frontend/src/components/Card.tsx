73a6149e8f1a074e1c69b3bd350574ed19435010
Alejandro Cano <alejandroyunes@outlook.com>
2024-07-24 01:05:24 -0500

    finish ui

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,7 +1,7 @@
 export default function Card() {
   // Please fetch the user data from the API
 
-  return <section className="w-full h-full p-4">
+  return <section className="w-full h-full">
     <article className="max-w-screen-xl mx-auto bg-gray-100 rounded-lg">
 
       <div className="flex items-center justify-between md:flex-row flex-col">
@@ -79,9 +79,31 @@ export default function Card() {
 
       </div>
 
-        <div className="flex space-x-2 p-2 bg-gray-200 rounded-bl-lg rounded-br-lg">
-        <button className="flex justify-center items-center px-3 py-2 bg-green-200 text-green-800 rounded-full">Accept <span className="ml-2"><img src="/icons/chevronDown.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
-        <button className="flex justify-center items-center px-3 py-2 bg-red-200 text-red-800 rounded-full">Reject <span className="ml-2"><img src="/icons/stopSvg.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
+      <div className="flex justify-between space-x-2 p-3 bg-gray-200 rounded-bl-lg rounded-br-lg">
+        <div className="flex flex-row hidden sm:flex">
+          <button className="flex justify-center items-center px-3 py-1 mr-2 bg-green-200 text-green-800 rounded-full">Accept <span className="ml-2"><img src="/icons/chevronDown.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
+          <button className="flex justify-center items-center px-3 py-1 mr-2 bg-red-200 text-red-800 rounded-full">Reject <span className="ml-2"><img src="/icons/stopSvg.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
+          <button className="flex justify-center items-center px-3 py-1 bg-gray-100 text-gray-500 rounded-full">Source: Direct</button>
+        </div>
+
+        <div className="sm:hidden">
+          <div className="relative group">
+            <button className="flex justify-center items-center px-3 py-1 mr-2 bg-blue-200 text-blue-800 rounded-full">Select <span className="ml-2"><img src="/icons/chevronDown.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
+            <div className="absolute hidden group-hover:flex flex-col bg-white shadow-lg rounded-lg p-2 bottom-full w-40">
+              <button className="flex justify-center items-center px-3 py-1 mr-2 bg-green-200 text-green-800 rounded-full hover:bg-green-300">Accept <span className="ml-2"><img src="/icons/chevronDown.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
+              <button className="flex justify-center items-center px-3 py-1 mt-2 mr-2 bg-red-200 text-red-800 rounded-full hover:bg-red-300">Reject <span className="ml-2"><img src="/icons/stopSvg.svg" alt="instagram icon" className="w-4 h-4" /></span></button>
+              <button className="flex justify-center items-center px-3 py-1 mt-2 bg-gray-100 text-gray-500 rounded-full hover:bg-gray-200">Source: Direct</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <button className="flex justify-center items-center px-3 py-1 bg-yellow-100 text-yellow-500 rounded-full">Overdue</button>
+          <p className="text-gray-700 text-sm ml-2">
+            Applied: 03/04/2024
+          </p>
+        </div>
+
       </div>
 
     </article>
